fd8df90dd125978ae2f4d19a07e2299998c666c7
iantpryor <iantpryor@gmail.com>
2015-01-16 12:58:33 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 var c = documnet.getElementById("mapCanvas");
 var ctx = c.getContext("2d");
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(0,0,150,75);
+ctx.beginPath();
+ctx.arc(95,50,40,0,2*Math.PI);
+ctx.stroke();
