8dc147d4350c7637c4163ffd1270a2fda98865a0
iantpryor <iantpryor@gmail.com>
2015-01-16 14:18:29 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@
     c.height = 540;
     var ctx = c.getContext("2d");
     ctx.fillStyle = "#FF0000";
-    for(x=0; x<100; x++){
-      for(y=0; y< 100; y++){
+    for(x=0; x<100; x+2){
+      for(y=0; y< 100; y+2){
         ctx.fillRect(x, y, 1, 1)
       }
     }
