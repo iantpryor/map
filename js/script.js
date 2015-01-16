52916c989a24d514c26109eab664db176f5a0c6e
iantpryor <iantpryor@gmail.com>
2015-01-16 13:58:59 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,8 @@
 (function(window, document, undefined){
   window.onload = init;
   function init(){
-    var r = 255;
-    var g = 255;
-    var b = 255;
-    var a = 255;
     var c = document.getElementById("mapCanvas");
     var ctx = c.getContext("2d");
     ctx.fillStyle = "#FF0000";
     ctx.fillRect(0,0,50,50);
-    
 })(window, document, undefined);
