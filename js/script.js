34d5b5f1b2b120a0a1b7472eac3bd3b40d009ace
iantpryor <iantpryor@gmail.com>
2015-01-16 14:12:22 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,6 +6,6 @@
     c.height = 540;
     var ctx = c.getContext("2d");
     ctx.fillStyle = "#FF0000";
-    ctx.fillRect(0,0,50,50);
+    ctx.fillRect(10,10,50,50);
   }
 })(window, document, undefined);
