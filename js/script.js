a725b2c61020422323c4591f892e4711451fb3e8
iantpryor <iantpryor@gmail.com>
2015-01-16 13:37:31 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,8 +3,15 @@
   function init(){
     var c = document.getElementById("mapCanvas");
     var ctx = c.getContext("2d");
-    ctx.beginPath();
-    ctx.arc(95,50,40,0,2*Math.PI);
-    ctx.stroke();
-  }
+    var id = ctx.createImageData(1,1);
+    var d = id.data;
+    d[0] = r;
+    d[1] = g;
+    d[2] = b;
+    d[3] = a;
+    for(x=0; x<50; x++){
+      for(y=0; y<50; y++){
+        ctx.putImageData(id, x, y);
+      }
+    }
 })(window, document, undefined);
