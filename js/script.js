32b6a72d63fbd62bf93e57b31c4646ba52b4f5b8
iantpryor <iantpryor@gmail.com>
2015-01-16 22:55:28 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
     c.width = 960;
     c.height = 540;
     var ctx = c.getContext("2d");
-    for(i=0; i<100; i+2){
+    for(i=0; i<100; i+=2){
       ctx.fillStyle = "#FF0000";
       ctx.fillRect(i, 50, 50, 50);
     }
