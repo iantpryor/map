375dbfb3773ae64f6f93eae91d34f85b0a5e608b
iantpryor <iantpryor@gmail.com>
2015-01-20 11:39:50 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,33 @@
-(function(window, document, undefined) {
-  window.onload = init;
-  function init() {
-    var c = document.getElementById("mapCanvas");
-    c.width = 960;
-    c.height = 540;
-    var ctx = c.getContext("2d");
-    var b = document.getElementById("startbtn").onclick = function() {
-      alert("button was clicked");
-      for(i=0; i<100; i++){
-        setTimeout(function(){
-          ctx.fillStyle = "#FF0000";
-          ctx.fillRect(i, 50, 50, 50);
-        },100);
-        
-      }
-    };
-  }
-})(window, document, undefined);
+var canvas = document.getElementById("mapCanvas");
+var c = canvas.getContext("2d");
+var myTimer;
+
+// set the dynamic outside the loop
+var dynamic = 10;
+
+//loop function
+function loop() {
+
+    // change dynamic
+    dynamic = dynamic * 1.1;
+    x = dynamic;
+    y = dynamic * 1.2;
+
+    // stop the the animation if it runs out-of-canvas
+    if (x > canvas.width || y > canvas.height) {
+        c.clearRect(0, 0, canvas.width, canvas.height);
+        clearInterval(myTimer);
+        alert("animation done!");
+    }
+
+    // clear the canvas for this loop's animation
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    c.fillStyle = '#87CEEB';
+
+    // draw
+    c.beginPath();
+    c.arc(x, y, 10, 0, Math.PI * 2, false);
+    c.fill();
+}
+
+$("#startbtn").click(function(){ dynamic=10; myTimer=setInterval(loop,20); });
