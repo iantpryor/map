bb9e0da5bf14fe2a164b8c623b671eb3b2ae10b8
iantpryor <iantpryor@gmail.com>
2015-01-22 15:56:43 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@
         //get the canvas
         var canvas = document.getElementById("mapcanvas");
         var c = canvas.getContext("2d");
-        c.imageSmoothingEnabled = false;
+        c.translate(0.5, 0.5);
         
         var imgData = c.createImageData(1, 1);
 
