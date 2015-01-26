58cfb1ab5a21c8bd346ec72641c1522e5df5e9fd
iantpryor <iantpryor@gmail.com>
2015-01-26 10:51:21 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,8 +13,8 @@
         }
         
         //function for finding if one point is a wall
-        function iswall(i,j){
-            if(i >= canvas.width || i < 0 || j >= canvas.height || j < 0){
+        function iswall(i,j) {
+            if(i >= canvas.width || i < 0 || j >= canvas.height || j < 0) {
                 return 1;
             }else{
                 return map[i][j];
@@ -28,28 +28,28 @@
             var j1 = j+1;
             var j2 = j-1;
             var neighbors = 0;
-            if(iswall(i1,j1) == 1){
+            if(iswall(i1,j1) == 1) {
                 neighbors++;
             }
-            if(iswall(i,j1) == 1){
+            if(iswall(i,j1) == 1) {
                 neighbors++;
             }
-            if(iswall(i2,j1) == 1){
+            if(iswall(i2,j1) == 1) {
                 neighbors++;
             }
-            if(iswall(i1,j) == 1){
+            if(iswall(i1,j) == 1) {
                 neighbors++;
             }
-            if(iswall(i2,j) == 1){
+            if(iswall(i2,j) == 1) {
                 neighbors++;
             }
-            if(iswall(i1,j2) == 1){
+            if(iswall(i1,j2) == 1) {
                 neighbors++;
             }
-            if(iswall(i,j2) == 1){
+            if(iswall(i,j2) == 1) {
                 neighbors++;
             }
-            if(iswall(i2,j2) == 1){
+            if(iswall(i2,j2) == 1) {
                 neighbors++;
             }
             return neighbors;
@@ -60,11 +60,11 @@
         var it = 1;
         function paint() {
             document.getElementById("iter").innerHTML = it;
-            if(it == 1){
+            if(it == 1) {
                 //fill the values
                 for(var i = 0; i < canvas.width; i++) {
                     for(var j = 0; j < canvas.height; j++) {
-                        if(Math.random() < .40){
+                        if(Math.random() < .40) {
                             map[i][j] = 1;
                         } else {
                             map[i][j] = 0;
@@ -73,16 +73,16 @@
                 }
             } else {
                 for(var i = 0; i < canvas.width; i++) {
-                    for(var j = 0; j < canvas.height; j++){
+                    for(var j = 0; j < canvas.height; j++) {
                         var nc = ncount(i,j);
-                        if(it<=4){
-                            if(nc >= 5 || nc <=2){
+                        if(it <= 4) {
+                            if(nc >= 5 || nc <= 2) {
                                 map[i][j] = 1;
                             }else{
                                 map[i][j] = 0;
                             }
                         }else{
-                            if(nc >=5){
+                            if(nc >= 4) {
                                 map[i][j] = 1;
                             }else{
                                 map[i][j] = 0;
@@ -94,8 +94,8 @@
             
             //paint the map
             for(var i = 0; i < canvas.width; i++) {
-                for(var j = 0; j< canvas.height; j++){
-                    if(map[i][j] == 1){
+                for(var j = 0; j < canvas.height; j++) {
+                    if(map[i][j] == 1) {
                         c.fillStyle = "#000000";
                         c.fillRect(i, j, 1, 1);
                     }else {
